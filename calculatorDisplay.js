de9088268459607b2f91929597d9500f11bf73c2
kalema1 <kalemar8@gmail.com>
2024-01-23 20:01:42 +0300

    feat(CalculatorState): create a CalculatorState class
    - created a CalculatorState class for our state of the calculator app.
    - added a constructor method with the inputButtons parameter.
    - added the appendNumber method that appends input  number buttons.
    - added the appendOperands method that appends operands
    - added style to the other-operators elements
    
    This update ensures our application cant caputure the state values of
    the calculator.

--- calculatorDisplay.js
+++ calculatorDisplay.js
@@ -1,2 +1,2 @@
 // This file contain the displays of my application.
-// it observes the state of the application
+// it observes the state of the application and displays the values
--- calculatorState.js
+++ calculatorState.js
@@ -1,1 +1,27 @@
 // This file to contain the state of my application
+//receives the buttton inpusts
+
+export class CalculatorState {
+  constructor(inputButtons) {
+    // input buttons
+    this.inputButtons = inputButtons;
+  }
+
+  /**
+   * apppend numbers on the application for operations
+   * @param {string} numbers - number string to be appended
+   * @return {string} -string of numbers
+   */
+  appendNumber(number) {
+    return (this.inputButtons += number);
+  }
+
+  /**
+   * apppend perands on the application for operations
+   * @param {string} operands - operand string to be appended
+   * @return {string}
+   */
+  appendOperands(operands) {
+    return (this.inputButtons += operands);
+  }
+}
